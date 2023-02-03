1111fa4cfecda0dc2e4f347f952b67d29fc482ed
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-02-03 23:12:48 +0700

    printing

--- Arithemtic.java
+++ Arithemtic.java
@@ -1,9 +1,9 @@
+import java.util.Scanner;
+
 public class Arithemtic {
     public static void main(String[] args) {
-        float a = 12.5f;
-        long b = 1234;
-
-        float result = a * b;
-
+        double a = (20 - 10 / 30);
+        System.out.println();
+        System.out.println((20 - 10) / 30);
     }
 }
