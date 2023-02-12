0b3b0ea4b54e60ab01ddfd71278719cad6975873
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-02-12 09:15:47 +0700

    add principle note

--- oops/Main.java
+++ oops/Main.java
@@ -2,14 +2,6 @@ package oops;
 
 public class Main {
     public static void main(String[] args) {
-        BoxV1 boxV1 = new BoxV1();
-        boxV1.d = 10.0d;
-        boxV1.w = 5.0d;
-        boxV1.h = 7.0d;
-
-        BoxV2 boxV2 = new BoxV2(4, 10, 6);
-        System.out.println(boxV2.volumn());
-        System.out.println(boxV2.surfaceArea());
-        System.out.print(boxV2);
+        System.out.println("Hello World");
     }
 }
