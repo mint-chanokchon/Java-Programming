2a24e85cf9e9cb5f6f1d37d94490258fdc859acc
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-02-14 23:12:07 +0700

    note: type of properties

--- oops/Main.java
+++ oops/Main.java
@@ -3,7 +3,6 @@ package oops;
 public class Main {
     public static void main(String[] args) {
         Circle c1 = new Circle();
-
         c1.radius = 5;
 
         System.out.println(c1.area());
