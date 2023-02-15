05f23610bde6d09fb2ffb73f1bc3763172821e8a
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-02-15 11:48:33 +0700

    Update Main.java

--- oops/Main.java
+++ oops/Main.java
@@ -2,9 +2,13 @@ package oops;
 
 public class Main {
     public static void main(String[] args) {
-        Ragtangle ragtangle = new Ragtangle(5, 5);
+        Ragtangle[] ragtangles = new Ragtangle[3];
+        ragtangles[0] = new Ragtangle(10, 5);
+        ragtangles[1] = new Ragtangle(12, 8);
+        ragtangles[2] = new Ragtangle(14, 11);
 
-        System.out.println(ragtangle.getLength());
-        System.out.println(ragtangle.getBreadth());
+        for (Ragtangle d: ragtangles) {
+            System.out.println(d.area());
+        }
     }
 }
